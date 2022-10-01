6a912662271da84206f3bc4ac7d9dbe72e1c534a
AfifuddinM <afifuddin15.am@gmail.com>
2022-10-01 21:34:13 +0800

    test

--- index.php
+++ index.php
@@ -6,10 +6,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Sadaqah Crowdfunding</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
-
+    <h1>hello</h1>
+    <div class="navbar">
+        <span class="titleCompany">Sadaqah Crowd Funding</span>
+        <div class="sbar"></div>
+        <a class="campaign" href="">Campaign</a>
+        <a class="about" href="">About</a>
+        <a class="cu" href="">Contact Us</a>
+        <a class="login" href=""><div></div></a>
+        <a class="register" href=""><div></div></a>
+    </div>
+    <div class="jumbotron"></div>
+    <div class="divider">
+        <span>SADAQAH CATEGORIES</span>
+    </div>
+    <div class="main">
+    <!-- Cash Border-->
+        <div class="d1"></div>
+    <!-- School Border-->
+        <di class="d2"v></div>
+    <!-- Services Border-->
+        <div class="d3"></div>
+    <!-- Services Border-->
+        <div class="d4"></div>
+    <!-- Calculate Border(Insert PHP Here)-->
+        <div class="d5"></div>
+    </div>
+    <div class="footer">
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quisquam iure, quam commodi, corporis animi sint laboriosam expedita odit sapiente velit ab deleniti tenetur dolorem suscipit illum dolore aspernatur reprehenderit.</span>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quisquam iure, quam commodi, corporis animi sint laboriosam expedita odit sapiente velit ab deleniti tenetur dolorem suscipit illum dolore aspernatur reprehenderit.</span>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quisquam iure, quam commodi, corporis animi sint laboriosam expedita odit sapiente velit ab deleniti tenetur dolorem suscipit illum dolore aspernatur reprehenderit.</span>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quisquam iure, quam commodi, corporis animi sint laboriosam expedita odit sapiente velit ab deleniti tenetur dolorem suscipit illum dolore aspernatur reprehenderit.</span>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quisquam iure, quam commodi, corporis animi sint laboriosam expedita odit sapiente velit ab deleniti tenetur dolorem suscipit illum dolore aspernatur reprehenderit.</span>
+    </div>
 </body>
 
 </html>
