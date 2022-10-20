5753c285c8387eb22218ab21df23243438e67c46
HaziqKhairi <ikhairulhaziq11@gmail.com>
2022-10-21 00:59:11 +0800

    Update .sql

--- sadaqahcrowdfunding.sql
+++ sadaqahcrowdfunding.sql
@@ -3,13 +3,19 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 01, 2022 at 12:18 PM
+-- Generation Time: Oct 20, 2022 at 06:58 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `sadaqahcrowdfunding`
@@ -95,6 +101,18 @@ CREATE TABLE `user` (
   `user_address` varchar(100) NOT NULL,
   `user_level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_username`, `user_password`, `user_name`, `user_email`, `user_phone`, `user_address`, `user_level`) VALUES
+(1, 'haziq', 'haziq', 'Haziq Khairi', 'ihaziqkhairi@gmail.com', '+60164005754', '8, Jalan Dewan Bahasa, Taman Hartamas, 06394 Pendang, Kedah', 1),
+(2, 'sufiya', 'sufiya', 'Nur Sufiya binti Abu Bakar', 'nursufiya@gmail.com', '+60196251237', '71, Jalan Pudu 20Q, SS8, 59352 Taman Desa, WP Kuala Lumpur', 3),
+(3, 'shazwan', 'shazwan', 'Shazwan bin Faizal', 'shazwan@yahoo.com', '+60193024231', '52, Jalan Petaling, Bandar Flora, 01524 Simpang Empat, Perlis Indera Kayangan', 2),
+(4, 'imran', 'imran', 'Imran Taufek bin Syed Dini', 'imrantaufek@gmail.com', '+60129512664', 'No. F-57-54, Jln 4/1, Taman Desa Delima, 72824 Tiroi, Negeri Sembilan', 2),
+(5, 'nurafifah', 'nurafifah', 'Nurafifah binti Hazzam', 'nurafifahhaz@gmail.com', '+60195827301', 'No. 447, Lorong 3/7, Pandan Meru, 23750 Rantau Abang, Terengganu Darul Iman', 2),
+(6, 'gopi', 'gopi', 'Gopinathan a/l Pragash', 'gopinathan@yahoo.com', '+60179620124', 'No. 1, Jalan Ampang 8/4, SS64O, 62040 Precinct 5, Putrajaya', 3);
 
 -- --------------------------------------------------------
 
@@ -189,7 +207,7 @@ ALTER TABLE `level`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `verification`
@@ -228,3 +246,7 @@ ALTER TABLE `verification`
   ADD CONSTRAINT `auditorId` FOREIGN KEY (`auditor_id`) REFERENCES `user` (`user_id`),
   ADD CONSTRAINT `campId` FOREIGN KEY (`campaign_id`) REFERENCES `campaign` (`campaign_id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
