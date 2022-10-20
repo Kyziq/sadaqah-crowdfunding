a4331bd7d170d9debbac6b4a3be113633c1c96a4
HaziqKhairi <ikhairulhaziq11@gmail.com>
2022-10-20 21:21:25 +0800

    Changed local file usage to CDN

--- index.php
+++ index.php
@@ -2,47 +2,111 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Sadaqah Crowdfunding</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
+
+    <title>Document</title>
 </head>
 
-<body>
-    <h1>hello</h1>
-    <div class="navbar">
-        <span class="titleCompany">Sadaqah Crowd Funding</span>
-        <div class="sbar"></div>
-        <a class="campaign" href="">Campaign</a>
-        <a class="about" href="">About</a>
-        <a class="cu" href="">Contact Us</a>
-        <a class="login" href=""><div></div></a>
-        <a class="register" href=""><div></div></a>
+<body></body>
+<header>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-mainColour">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Sadaqah Crowd Funding</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Campaign
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Cash</a></li>
+                            <li><a class="dropdown-item" href="#">School</a></li>
+                            <li><a class="dropdown-item" href="#">Services</a></li>
+                            <li><a class="dropdown-item" href="#">Facilitator</a></li>
+                        </ul>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <a href=""><button id="twobtn" class="btn btn-outline-dark me-2" type="submit">Login</button></a>
+                    <a href=""><button id="twobtn" class="btn btn-outline-dark me-2" type="submit">Register</button></a>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <!-- End Of Navbar -->
+</header>
+<div class="jumbotron"></div>
+<div class="seperater mb-4">
+    <h1>Sadaqah Categories</h1>
+</div>
+<main>
+    <div>
+        <div class="row align-items-start">
+            <div class="col">
+                <span>Remember that the happiest people are not those getting more, but those giving more.</span>
+            </div>
+            <div class="col-8">
+                <div class="row align-items-center">
+                    <div class="col cat1">Cash</div>
+                    <div class="col cat2">School</div>
+                    <div class="col cat3">Services</div>
+                    <div class="col cat4">Facilitator</div>
+                    <div class="w-100"></div>
+                    <!-- insert PHP Here -->
+                    <div class="col phpnumber mb-5" style="text-align:center ;">
+                        <div class="row">
+                            <div class="col">
+                                <div class="row"><strong>RM 10000000</strong></div>
+                                <div class="row"><span>Raised</span></div>
+                            </div>
+                            <div class="col">
+                                <div class="row"><strong>24550</strong></div>
+                                <div class="row"><span>Givers</span></div>
+                            </div>
+                            <div class="col">
+                                <div class="row"><strong>50 </strong></div>
+                                <div class="row"><span>Campaigns</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <span>No one has ever become poor from giving.</span>
+            </div>
+        </div>
+</main>
+<footer>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: #4E944F;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, molestiae nulla ea nostrum fugit sint quam voluptatibus eligendi atque at minima officiis sapiente modi, nesciunt ut doloribus quidem impedit dicta quae praesentium? Earum quo dolores hic mollitia maxime quae veniam dolorum explicabo similique magnam nostrum a quos, sit itaque quam dicta minima natus reprehenderit, harum, cupiditate officiis? Dignissimos nostrum reprehenderit quam a error quod eum ab, atque vitae dolorum, dicta aperiam voluptates est odio commodi quis doloribus suscipit velit aspernatur nisi maiores veniam dolorem repellendus! Numquam fugiat accusantium ratione ipsam at quasi animi architecto sunt unde? Cupiditate cumque atque dolorem ullam quam? Modi officiis itaque placeat nesciunt soluta eius, ullam fugit sit quaerat sequi cumque dolor? Nobis nemo in pariatur unde doloribus modi alias reiciendis, error aspernatur? Excepturi id corrupti ullam quam inventore veritatis tempore! Eos, amet quod facilis aut neque ratione consequuntur doloribus esse vel provident rerum. Aliquam facilis accusamus recusandae nulla vero! Neque in soluta numquam. Quis quia repellendus assumenda quasi est earum esse culpa? Aut rem distinctio earum neque adipisci, aspernatur, laudantium ea totam esse dolores fugiat officiis ipsa? Dolore maiores laudantium repellat tenetur esse velit adipisci voluptas exercitationem, nam possimus soluta ipsum veritatis necessitatibus cupiditate amet!
+        <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
     </div>
-    <div class="jumbotron"></div>
-    <div class="divider">
-        <span>SADAQAH CATEGORIES</span>
-    </div>
-    <div class="main">
-    <!-- Cash Border-->
-        <div class="d1"></div>
-    <!-- School Border-->
-        <di class="d2"v></div>
-    <!-- Services Border-->
-        <div class="d3"></div>
-    <!-- Services Border-->
-        <div class="d4"></div>
-    <!-- Calculate Border(Insert PHP Here)-->
-        <div class="d5"></div>
-    </div>
-    <div class="footer">
-        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quisquam iure, quam commodi, corporis animi sint laboriosam expedita odit sapiente velit ab deleniti tenetur dolorem suscipit illum dolore aspernatur reprehenderit.</span>
-        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quisquam iure, quam commodi, corporis animi sint laboriosam expedita odit sapiente velit ab deleniti tenetur dolorem suscipit illum dolore aspernatur reprehenderit.</span>
-        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quisquam iure, quam commodi, corporis animi sint laboriosam expedita odit sapiente velit ab deleniti tenetur dolorem suscipit illum dolore aspernatur reprehenderit.</span>
-        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quisquam iure, quam commodi, corporis animi sint laboriosam expedita odit sapiente velit ab deleniti tenetur dolorem suscipit illum dolore aspernatur reprehenderit.</span>
-        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quisquam iure, quam commodi, corporis animi sint laboriosam expedita odit sapiente velit ab deleniti tenetur dolorem suscipit illum dolore aspernatur reprehenderit.</span>
-    </div>
+    <!-- Copyright -->
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 
 </html>
