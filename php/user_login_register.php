44c8a321f1b869917e84ac599ff3a1497665f1a4
AfifuddinM <afifuddin15.am@gmail.com>
2022-10-27 22:39:36 +0800

    Update user_login_register.php

--- php/user_login_register.php
+++ php/user_login_register.php
@@ -34,11 +34,45 @@
     <?php
     }
     ?>
-    <div class="container p-5 my-5">
-        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
-        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#registerModal">Register</button>
-    </div>
-
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-mainColour">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Sadaqah Crowd Funding</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Campaign
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Cash</a></li>
+                            <li><a class="dropdown-item" href="#">School</a></li>
+                            <li><a class="dropdown-item" href="#">Services</a></li>
+                            <li><a class="dropdown-item" href="#">Facilitator</a></li>
+                        </ul>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-primary me-2" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
+                    <button class="btn btn-primary me-2" data-bs-toggle="modal" data-bs-target="#registerModal">Register</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+  
     <!-- Login Modal -->
     <div class="container p-5 my-5">
         <div class="modal fade" id="loginModal">
@@ -165,7 +199,11 @@
             </div>
         </div>
     </div>
-
+    
+    <!-- main content -->
+    <div class="jumbotron">
+        <h1 class=text-center>hello World</h1>
+    </div>
     <script>
         // Toggle password visibility
         !(function($) {
