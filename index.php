48c00a0bc3007f4bddbe14e525d240ddad195e39
AfifuddinM <afifuddin15.am@gmail.com>
2022-10-28 00:01:38 +0800

    update index

--- index.php
+++ index.php
@@ -5,18 +5,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/style.css">
+    <title>Login Page</title>
 
-    <title>Document</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <!-- Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/dashboard.css">
 </head>
 
-<body></body>
-<header>
+<body>
+    <!-- Relaunch modal -->
+    <?php
+    if ((isset($_GET["login"]) && $_GET["login"] == 'failed') || (isset($_GET["passw"]) && $_GET["passw"] == 'failed')) {
+    ?>
+        <script type="text/javascript">
+            window.onload = () => {
+                $("#loginModal").modal("show");
+            };
+        </script>
+    <?php
+    }
+    ?>
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg bg-mainColour">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Sadaqah Crowd Funding</a>
+            <a class="navbar-brand" href="#"><strong>Sadaqah <br>Crowd Funding</strong></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -43,71 +62,239 @@
                         <a class="nav-link" href="#">Contact Us</a>
                     </li>
                 </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <a href=""><button id="twobtn" class="btn btn-outline-dark me-2" type="submit">Login</button></a>
-                    <a href=""><button id="twobtn" class="btn btn-outline-dark me-2" type="submit">Register</button></a>
+                <form class="d-flex " role="search">
+                    <input class="search form-control me-2 w-100" type="search" placeholder="Search" aria-label="Search">
+                    
                 </form>
+                <button class="btn btn-outline-success me-2" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
+                <button class="btn btn-outline-success me-2" data-bs-toggle="modal" data-bs-target="#registerModal">Register</button>
             </div>
         </div>
     </nav>
-    <!-- End Of Navbar -->
-</header>
-<div class="jumbotron"></div>
-<div class="seperater mb-4">
-    <h1>Sadaqah Categories</h1>
-</div>
-<main>
-    <div>
-        <div class="row align-items-start">
-            <div class="col">
-                <span>Remember that the happiest people are not those getting more, but those giving more.</span>
+    <main>
+        <!-- main content -->
+    <div class="jumbotron"></div>
+    <!-- Php Numbers -->
+    <div class="calculations d-flex">
+        <div class="text me-auto pt-3 ms-5">
+            <h3>You have the ability to make a difference.</h3>
+            <p>Every ringgit counts and has aided those in need</p>
+        </div>
+        <div class="money d-flex flex-row p-2">
+            <div class="raised border mx-2 px-5 rounded-3">
+                <div class="cal"></div>
+                <p>Raised</p>
             </div>
-            <div class="col-8">
-                <div class="row align-items-center">
-                    <div class="col cat1">Cash</div>
-                    <div class="col cat2">School</div>
-                    <div class="col cat3">Services</div>
-                    <div class="col cat4">Facilitator</div>
-                    <div class="w-100"></div>
-                    <!-- insert PHP Here -->
-                    <div class="col phpnumber mb-5" style="text-align:center ;">
-                        <div class="row">
-                            <div class="col">
-                                <div class="row"><strong>RM 10000000</strong></div>
-                                <div class="row"><span>Raised</span></div>
-                            </div>
-                            <div class="col">
-                                <div class="row"><strong>24550</strong></div>
-                                <div class="row"><span>Givers</span></div>
-                            </div>
-                            <div class="col">
-                                <div class="row"><strong>50 </strong></div>
-                                <div class="row"><span>Campaigns</span></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="givers border mx-2 px-5 rounded-3">
+                <div class="cal"></div>
+                <p>Givers</p>
             </div>
-            <div class="col">
-                <span>No one has ever become poor from giving.</span>
+            <div class="campaigns border mx-2 px-5 rounded-3">
+                <div class="cal"></div>
+                <p>Campaigns</p>
             </div>
         </div>
-</main>
-<footer>
-    <!-- Grid container -->
-
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: #4E944F;">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, molestiae nulla ea nostrum fugit sint quam voluptatibus eligendi atque at minima officiis sapiente modi, nesciunt ut doloribus quidem impedit dicta quae praesentium? Earum quo dolores hic mollitia maxime quae veniam dolorum explicabo similique magnam nostrum a quos, sit itaque quam dicta minima natus reprehenderit, harum, cupiditate officiis? Dignissimos nostrum reprehenderit quam a error quod eum ab, atque vitae dolorum, dicta aperiam voluptates est odio commodi quis doloribus suscipit velit aspernatur nisi maiores veniam dolorem repellendus! Numquam fugiat accusantium ratione ipsam at quasi animi architecto sunt unde? Cupiditate cumque atque dolorem ullam quam? Modi officiis itaque placeat nesciunt soluta eius, ullam fugit sit quaerat sequi cumque dolor? Nobis nemo in pariatur unde doloribus modi alias reiciendis, error aspernatur? Excepturi id corrupti ullam quam inventore veritatis tempore! Eos, amet quod facilis aut neque ratione consequuntur doloribus esse vel provident rerum. Aliquam facilis accusamus recusandae nulla vero! Neque in soluta numquam. Quis quia repellendus assumenda quasi est earum esse culpa? Aut rem distinctio earum neque adipisci, aspernatur, laudantium ea totam esse dolores fugiat officiis ipsa? Dolore maiores laudantium repellat tenetur esse velit adipisci voluptas exercitationem, nam possimus soluta ipsum veritatis necessitatibus cupiditate amet!
-        <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
     </div>
-    <!-- Copyright -->
-</footer>
-<!-- CDN -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <!-- End of Php Numbers -->
+    <div class="aboutUs">
+        <h2 class="mt-5 mb-5 text-center">About Us</h2>
+        <div class="cards d-flex justify-content-center">
+
+                <div class="card mx-2" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                         <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                 </div>
+
+                <div class="card mx-2" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                         <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                 </div>
+
+                <div class="card mx-2" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                         <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                 </div>
+        </div>
+    </div>
+
+    </main>
+    <!-- Login Modal -->
+    <div class="container p-5 my-5">
+        <div class="modal fade" id="loginModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <!-- Form -->
+                    <form action="user_login_action.php" method="post">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5">Login Form</h1>
+                            <a class="btn-close" data-bs-dismiss="modal" data-bs-target="#loginModal"></a>
+                        </div>
+                        <div class="modal-body">
+                            <a href="../index.php">
+                                <img src="https://i0.wp.com/www.zakatkedah.com.my/wp-content/uploads/2021/05/Logo-Zakat-Kedah-Baru-2021.png?w=696&ssl=1" class="mx-auto d-block" style="max-width:40%;" alt="Logo">
+                            </a>
+
+                            <!-- Input -->
+                            <div class="form-group mb-3">
+                                <label for="user_username" class="form-label">Username</label>
+                                <div class="input-group">
+                                    <div class="input-group-text">
+                                        <i class="fas fa-user"></i>
+                                    </div>
+                                    <input type="text" class="form-control" id="user_username" name="user_username" placeholder="Enter username" required>
+                                </div>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label for="user_password" class="form-label">Password</label>
+                                <div class="input-group">
+                                    <div class="input-group-text">
+                                        <i class="fas fa-lock"></i>
+                                    </div>
+                                    <input type="password" class="form-control" placeholder="Enter your password" name="user_password" required />
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <?php
+                                /* Check for wrong password */
+                                if (isset($_GET["login"]) && $_GET["login"] == 'failed') {
+                                    echo '<span style="color: red;">Account does not exist!</span>';
+                                } else if (isset($_GET["passw"]) && $_GET["passw"] == 'failed') {
+                                    echo '<span style="color: red;">Wrong password!</span>';
+                                }
+                                ?>
+                            </div>
+
+                            Don't have an account?
+                            <a href="#registerModal" data-target="#registerModal" data-bs-toggle="modal" title="Go to Register Form">
+                                Register Now
+                            </a>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary" name="login-button">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Register Modal -->
+    <div class="container p-5 my-5">
+        <div class="modal fade registerModal" id="registerModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <!-- Form -->
+                    <form action="user_register_save.php" method="post">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5">Registration Form</h1>
+                            <a class="btn-close" data-bs-dismiss="modal" data-bs-target="#registerModal"></a>
+                        </div>
+                        <div class="modal-body">
+                            <a href="../index.php">
+                                <img src="https://i0.wp.com/www.zakatkedah.com.my/wp-content/uploads/2021/05/Logo-Zakat-Kedah-Baru-2021.png?w=696&ssl=1" class="mx-auto d-block" style="max-width:40%;" alt="Logo">
+                            </a>
+                            <!-- Input -->
+                            <div class="form-group mb-2">
+                                <label for="username" class="form-label">Username</label>
+                                <input type="text" class="form-control" id="username" name="username" required />
+                            </div>
+
+                            <div class="form-group mb-2">
+                                <label for="password" class="form-label">Password</label>
+                                <div class="input-group">
+                                    <input type="password" class="form-control" id="password" name="password" data-toggle="password" required />
+                                    <div class="input-group-text">
+                                        <i class="fa fa-eye" id="toggleNewPassword" style="cursor: pointer"></i>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group mb-2">
+                                <label for="name" class="form-label">Full Name</label>
+                                <input type="text" class="form-control" id="name" name="name" required />
+                            </div>
+
+                            <div class="form-group mb-2">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="email" name="email" required />
+                            </div>
+
+                            <div class="form-group mb-2">
+                                <label for="phone" class="form-label">Phone Number</label>
+                                <input type="number" step=1 class="form-control" id="phone" name="phone" required />
+                            </div>
+
+                            <div class="form-group mb-2">
+                                <label for="address" class="form-label">Address</label>
+                                <textarea type="text" class="form-control" id="address" name="address" rows="3" required></textarea>
+                            </div>
+
+                            Already a member?
+                            <a href="#loginModal" data-target="#loginModal" data-bs-toggle="modal" title="Go to Login Form">
+                                Login Now
+                            </a>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary" name="register-button">Register</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
+    <script>
+        // Toggle password visibility
+        !(function($) {
+            //eyeOpenClass: 'fa-eye',
+            //eyeCloseClass: 'fa-eye-slash',
+            "use strict";
+
+            $(function() {
+                $('[data-toggle="password"]').each(function() {
+                    var input = $(this);
+                    var eye_btn = $(this).parent().find(".input-group-text");
+                    eye_btn.css("cursor", "pointer").addClass("input-password-hide");
+                    eye_btn.on("click", function() {
+                        if (eye_btn.hasClass("input-password-hide")) {
+                            eye_btn
+                                .removeClass("input-password-hide")
+                                .addClass("input-password-show");
+                            eye_btn
+                                .find(".fa")
+                                .removeClass("fa-eye")
+                                .addClass("fa-eye-slash");
+                            input.attr("type", "text");
+                        } else {
+                            eye_btn
+                                .removeClass("input-password-show")
+                                .addClass("input-password-hide");
+                            eye_btn
+                                .find(".fa")
+                                .removeClass("fa-eye-slash")
+                                .addClass("fa-eye");
+                            input.attr("type", "password");
+                        }
+                    });
+                });
+            });
+        })(window.jQuery);
+    </script>
 </body>
 
 </html>
