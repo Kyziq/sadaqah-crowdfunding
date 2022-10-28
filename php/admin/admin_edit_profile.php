62ca7d214d3ce3181c0ea6375a8deb03afe90bfa
Haziq Khairi <ikhairulhaziq11@gmail.com>
2022-10-28 16:26:29 +0800

    UPDATE: Edit profile layout and bug fixes

--- php/admin/admin_edit_profile.php
+++ php/admin/admin_edit_profile.php
@@ -153,11 +153,11 @@
         </div>
         <section class="section">
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col-lg-6">
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Account Details</h5>
-                            <form action="admin_edit_profile_save.php" method="POST" class="row g-2">
+                            <form action="admin_edit_profile_save.php" method="POST" class="row g-2 col-lg-12">
                                 <!-- Input -->
                                 <div class="form-group mb-2">
                                     <div class="form-floating">
@@ -194,14 +194,17 @@
                                     </div>
                                 </div>
 
-                                <div class="form-group mb-4">
-                                    <button type="Submit" class="btn btn-primary" value="Submit" name="edit-action-button">Save</button>
-                                </div>
+                                <button type="Submit" class="btn btn-primary" value="Submit" name="edit-action-button">Save</button>
                             </form>
-
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Password Settings</h5>
                             <!-- Password Change Form -->
-                            <p class="h5">Password Settings</p>
-                            <form action="admin_edit_profile_save.php" method="POST">
+                            <form action="admin_edit_profile_save.php" method="POST" class="row g-2 col-lg-12">
                                 <!-- Input -->
                                 <div class="form-group mb-2">
                                     <label for="currentPassword">Current Password</label>
@@ -236,9 +239,7 @@
                                     <span id='passwordCheckMessage'></span>
                                 </div>
 
-                                <div class="form-group mb-2">
-                                    <button type="Submit" class="btn btn-primary" value="Submit" name="edit-password-button">Change Password</button>
-                                </div>
+                                <button type="Submit" class="btn btn-primary" value="Submit" name="edit-password-button">Change Password</button>
                             </form>
                         </div>
                     </div>
--- php/auditor/auditor_edit_profile.php
+++ php/auditor/auditor_edit_profile.php
@@ -104,11 +104,11 @@
         </div>
         <section class="section">
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col-lg-6">
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Account Details</h5>
-                            <form action="auditor_edit_profile_save.php" method="POST">
+                            <form action="auditor_edit_profile_save.php" method="POST" class="row g-2 col-lg-12">
                                 <!-- Input -->
                                 <div class="form-group mb-2">
                                     <div class="form-floating">
@@ -145,14 +145,17 @@
                                     </div>
                                 </div>
 
-                                <div class="form-group mb-4">
-                                    <button type="Submit" class="btn btn-primary" value="Submit" name="edit-action-button">Save</button>
-                                </div>
+                                <button type="Submit" class="btn btn-primary" value="Submit" name="edit-action-button">Save</button>
                             </form>
-
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Password Settings</h5>
                             <!-- Password Change Form -->
-                            <p class="h5">Password Settings</p>
-                            <form action="auditor_edit_profile_save.php" method="POST" class="row g-2">
+                            <form action="auditor_edit_profile_save.php" method="POST" class="row g-2 col-lg-12">
                                 <!-- Input -->
                                 <div class="form-group mb-2">
                                     <label for="currentPassword">Current Password</label>
@@ -187,9 +190,7 @@
                                     <span id='passwordCheckMessage'></span>
                                 </div>
 
-                                <div class="form-group mb-2">
-                                    <button type="Submit" class="btn btn-primary" value="Submit" name="edit-password-button">Change Password</button>
-                                </div>
+                                <button type="Submit" class="btn btn-primary" value="Submit" name="edit-password-button">Change Password</button>
                             </form>
                         </div>
                     </div>
