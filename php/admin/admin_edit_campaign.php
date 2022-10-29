0b845f753638d6d4c69bca7b127d71115dc67157
Haziq Khairi <ikhairulhaziq11@gmail.com>
2022-10-29 09:11:07 +0800

    ADD: Hidden campaign ID input

--- php/admin/admin_edit_campaign.php
+++ php/admin/admin_edit_campaign.php
@@ -234,6 +234,8 @@
                                                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                                 </div>
                                                                 <div class="modal-body">
+                                                                    <input type="hidden" name="id" value="<?php echo $r['campaign_id']; ?>">
+
                                                                     <div class="form-group mb-2">
                                                                         <label for="name" class="form-label">Campaign Name</label>
                                                                         <input type="text" class="form-control" name="name" value="<?php echo $r['campaign_name']; ?>">
