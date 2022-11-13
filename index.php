954d9c35f0b6f2fd6e9b206fccc103b64bd91c94
Haziq Khairi <ikhairulhaziq11@gmail.com>
2022-11-13 13:18:25 +0800

    UPDATE

--- index.php
+++ index.php
@@ -11,13 +11,15 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.4/swiper-bundle.min.css" integrity="sha512-pJrGHWDVOeiy4UkMtHu0fpD8oLLssFcaW0fsVXUkA1/jDLopa554Z1AZo5SKtekHnnmyat0ipiP0snKDrt0GNg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./css/homepage.css">
     <link rel="stylesheet" href="./css/custom-css.css">
 
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
     <script src="https://unpkg.com/counterup2@2.0.2/dist/index.js"> </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.4/swiper-bundle.min.js" integrity="sha512-k2o1KZdvUi59PUXirfThShW9Gdwtk+jVYum6t7RmyCNAVyF9ozijFpvLEWmpgqkHuqSWpflsLf5+PEW6Lxy/wA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 </head>
 
 <body>
@@ -205,7 +207,7 @@
                 <div class="cards d-flex justify-content-center pb-4">
                     <?php
                     $campaign_status = 1; // Accepted campaign
-                    $query = "SELECT * FROM campaign camp, category cat WHERE camp.campaign_category_id = cat.category_id AND camp.campaign_status=? ORDER BY camp.campaign_id DESC limit 3";
+                    $query = "SELECT * FROM campaign camp, category cat WHERE camp.campaign_category_id=cat.category_id AND camp.campaign_status=? ORDER BY camp.campaign_id desc LIMIT 3";
                     $stmt = $con->prepare($query);
                     $stmt->bind_param("i", $campaign_status);
                     $stmt->execute();
@@ -285,6 +287,7 @@
                         $index++;
                     }
                     ?>
+
                 </div>
             </div>
         </section>
